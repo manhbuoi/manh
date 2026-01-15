775998b89bd233bf4897b949564a1754a622096a
manh <manhb282@gmail.com>
2026-01-15 13:28:43 +0700

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace cuahanggiay.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
